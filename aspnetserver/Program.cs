2418731bdef4b84f23d1ca5a23d1b87c8af97f1e
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2022-08-13 13:20:16 -0500

    Add project files.

---
+++ aspnetserver/Program.cs
@@ -0,0 +1,75 @@
+using aspnetserver.Data;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+        });
+});
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NET React Tutorial", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
+{
+    swaggerUIOptions.DocumentTitle = "ASP.NET React Tutorial";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API serving a Post model");
+    swaggerUIOptions.RoutePrefix = String.Empty;
+});
+
+app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
+
+app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync())
+    .WithTags("Posts Endpoints");
+
+app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
+{
+    Post postToReturn = await PostsRepository.GetPostByIdAsync(postId);
+
+    return postToReturn != null ? Results.Ok(postToReturn) : Results.BadRequest();
+})
+    .WithTags("Posts Endpoints");
+
+app.MapPost("/create-post", async (Post postToCreate) =>
+{
+    bool createdSuccessful = await PostsRepository.CreatePostAsync(postToCreate);
+
+    return createdSuccessful ? Results.Ok("Create successful") : Results.BadRequest();
+})
+    .WithTags("Posts Endpoints");
+
+app.MapPut("/update-post", async (Post postToUpdate) =>
+{
+    bool updatedSuccessful = await PostsRepository.UpdatePostAsync(postToUpdate);
+
+    return updatedSuccessful ? Results.Ok("Update successful") : Results.BadRequest();
+})
+    .WithTags("Posts Endpoints");
+
+app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
+{
+    bool deleteSuccessful = await PostsRepository.DeletePostAsync(postId);
+
+    return deleteSuccessful ? Results.Ok("Delete successful") : Results.BadRequest();
+})
+    .WithTags("Posts Endpoints");
+
+app.Run();
